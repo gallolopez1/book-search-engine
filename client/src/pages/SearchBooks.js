dc36862147de95d34475f02a5588cdc059553844
Rooster <gallolopez1@hotmail.com>
2022-02-27 17:41:32 -0500

    updated signup and login forms

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
